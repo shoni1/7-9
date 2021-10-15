cfeb7e9a7e8673b6eb73d570512cfad9157612d5
unknown <shoni.zhansagimova@gmail.com>
2021-10-15 15:44:33 +0300

    first commit

---
+++ 7-9.py
@@ -0,0 +1,22 @@
+def chislonaoborot(x):
+    strok = x
+    x = abs(x)
+    b = x
+    d = 0
+    while b > 0:
+        a = x % 10
+        b = x // 10
+        d = d * 10 + a
+        x = b
+    if strok > 0:
+        print("Обратное число: " + str(d))
+    else:
+        print("Обратное число: " + str(d * -1))
+    return ""
+
+
+try:
+    x = int(input("Введите целое число: "))
+    print(chislonaoborot(x))
+except ValueError:
+    print("Не пытайтесь меня обмануть. Введите целое число")
